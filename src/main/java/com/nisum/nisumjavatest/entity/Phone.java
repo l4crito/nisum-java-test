d0d838bceafd29c479666583418fcfac5a4ce28d
Christian Caicedo <christian.caicedo@grainger.com>
2023-04-17 14:23:38 -0500

    Added lombook for getters and setters, Added User and phone Entity

--- src/main/java/com/nisum/nisumjavatest/entity/Phone.java
+++ src/main/java/com/nisum/nisumjavatest/entity/Phone.java
@@ -1,2 +1,28 @@
-package com.nisum.nisumjavatest.entity;public class Phone {
+package com.nisum.nisumjavatest.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "phone")
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "number")
+    private String number;
+
+    @Column(name = "citycode")
+    private String cityCode;
+
+    @Column(name = "countrycode")
+    private String countryCode;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // Constructors, getters and setters
+
 }
--- src/main/java/com/nisum/nisumjavatest/entity/User.java
+++ src/main/java/com/nisum/nisumjavatest/entity/User.java
@@ -1,2 +1,45 @@
-package com.nisum.nisumjavatest.entity;public class User {
+package com.nisum.nisumjavatest.entity;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Phone> phones;
+
+    @Column(nullable = false)
+    private boolean isActive;
+
+    @Column(nullable = false)
+    private LocalDateTime created;
+
+    @Column(nullable = false)
+    private LocalDateTime modified;
+
+    @Column(nullable = false)
+    private LocalDateTime lastLogin;
+
+    @Column(nullable = false)
+    private String token;
+
+    // Getters and setters
 }
