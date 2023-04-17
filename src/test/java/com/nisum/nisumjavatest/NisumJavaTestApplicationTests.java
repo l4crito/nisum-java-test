98b9109ff00072ebb97d2b731e69d94cd9d58fed
Christian Caicedo <christian.caicedo@grainger.com>
2023-04-17 12:02:49 -0500

    Project dependencies

---
+++ src/test/java/com/nisum/nisumjavatest/NisumJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.nisumjavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NisumJavaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
